018697df319b9f38d8f73738d3864e1ebe28d198
Regis <X-lem@hotmail.com>
2017-12-04 15:13:37 -0700

    test commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg'),
+    app = express();
+
+var conString = "postgres://simpleuser:pizel71@localhost/RecipleBook";
+app.engine('dust',cons.dust);
+
+// Set default ext .dust
+app.set('view engine', 'dust');
+app.set('views' __dirname + '/views');
+
+// Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
